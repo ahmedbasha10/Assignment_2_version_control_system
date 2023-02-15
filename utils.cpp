9b1eab3d3af5817cb58caba7e40a3878633c42d0
ahmedbasha10 <bashamido6060@gmail.com>
2023-02-15 15:52:33 +0200

    add average

--- utils.cpp
+++ utils.cpp
@@ -14,9 +14,11 @@ int get_sum(vector<int> numbers) {
 
 float get_average(vector<int> numbers) {
     int sum = 0;
+    
     for (int i = 0; i < numbers.size(); i++) {
         sum += numbers[i];
     }
+
     return (float)sum / numbers.size();
 }
 
