8720dc24ee420507d4fd7a7af2019c45d390c755
ahmedbasha10 <bashamido6060@gmail.com>
2023-02-15 15:09:18 +0200

    add get_sum function

---
+++ utils.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+int getSum(vector<int> numbers) {
+    int sum = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
--- utils.h
+++ utils.h
@@ -3,14 +3,9 @@
 
 #include <vector>
 
-<<<<<<< HEAD
-int get_sum(std::vector<int> numbers);
-=======
 using namespace std;
 
 int get_sum(vector<int> numbers);
 
-int get_min(vector<int> numbers);
->>>>>>> average_branch
 
 #endif
