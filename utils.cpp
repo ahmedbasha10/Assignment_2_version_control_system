ce4eb1a55970d2e92b828359fdb94bc4a80a3d74
ahmedbasha10 <bashamido6060@gmail.com>
2023-02-15 15:46:19 +0200

    aa

--- utils.cpp
+++ utils.cpp
@@ -10,13 +10,6 @@ int getSum(vector<int> numbers) {
     return sum;
 }
 
-float get_average(vector<int> numbers) {
-    int sum = 0;
-    for (int i = 0; i < numbers.size(); i++) {
-        sum += numbers[i];
-    }
-    return (float)sum / numbers.size();
-}
 
 int get_min(vector<int> numbers) {
     int min = numbers[0];
